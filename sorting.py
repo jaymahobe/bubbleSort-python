9dc36382d3e68479b56e0339a6ada5326dca9720
jaymahobe <jaymahobe3312@gmail.com>
2023-09-05 14:49:52 +0530

    rm comments

--- sorting.py
+++ sorting.py
@@ -2,13 +2,12 @@ def bubble_sort(array):
     n = len(array)
     for i in range(n):
         for j in range(0, n-i-1):
-# this loop is check the 1st digit and 2nd digit which is greater (j+1 is not a adding thing it is for check the digits perticular way)
+
             if array[j] > array[j+1]:
 
                 # this loop is for swapping
                 array[j], array[j+1] = array[j+1], array[j]
 
-# given number is 1 to 100
 numbers = [ 8, 9, 4, 6, 1, 3, 2, 5, 7]
 
 # Call the bubble_sort function to sort the numbers
